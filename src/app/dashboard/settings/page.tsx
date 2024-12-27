95be14852a354b4f55b42e8214021a9777555105
Nopass0 <gal.bogdan2015@gmail.com>
2024-12-28 00:49:13 +0500

    new

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -94,7 +94,7 @@ export default function SettingsPage() {
             </CardContent>
           </Card>
         )}
-        <GeminiTokenCard />
+        {/* <GeminiTokenCard /> */}
         <DeviceTokens />
       </motion.div>
     </div>
